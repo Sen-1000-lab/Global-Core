d0f6267ae8e05a8bd77b7f0c37c92700585a8fcc
Sen-xSpk-lab <t31075049@nara-su.ac.jp>
2026-06-23 08:07:52 +0900

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -3,16 +3,17 @@ from fastapi import HTTPException
 
 from config import API_KEY
 
-
 async def verify_api_key(
-    x_api_key: str = Header(None)
+x_api_key: str = Header(None)
 ):
 
-    if x_api_key != API_KEY:
+```
+if x_api_key != API_KEY:
 
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid API Key"
-        )
+    raise HTTPException(
+        status_code=401,
+        detail="Invalid API Key"
+    )
 
-    return True
+return True
+```
