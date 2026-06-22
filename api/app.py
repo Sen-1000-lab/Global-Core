52c5bed2afb4ba3f9b7a653d4f427911e91a91bf
Sen-xSpk-lab <t31075049@nara-su.ac.jp>
2026-06-23 08:09:44 +0900

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,30 +1,39 @@
 from fastapi import FastAPI
 
 from routes.users import router as user_router
+from routes.friends import router as friend_router
 
 app = FastAPI(
-    title="Globally Core API"
+title="Globally Core API"
 )
 
 app.include_router(
-    user_router,
-    prefix="/api/users",
-    tags=["Users"]
+user_router,
+prefix="/api/users",
+tags=["Users"]
 )
 
+app.include_router(
+friend_router,
+prefix="/api/friends",
+tags=["Friends"]
+)
 
 @app.get("/")
 async def root():
 
-    return {
-        "success": True,
-        "service": "Globally Core"
-    }
-
+```
+return {
+    "success": True,
+    "service": "Globally Core"
+}
+```
 
 @app.get("/health")
 async def health():
 
-    return {
-        "status": "ok"
-    }
+```
+return {
+    "status": "ok"
+}
+```
