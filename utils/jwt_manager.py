00c743efd47555d199c95a8d69cee48a2d153522
Sen-xSpk-lab <t31075049@nara-su.ac.jp>
2026-06-23 08:07:27 +0900

    Update jwt_manager.py

--- utils/jwt_manager.py
+++ utils/jwt_manager.py
@@ -1,38 +1,53 @@
 from jose import jwt
+from jose import JWTError
+
 from datetime import datetime
 from datetime import timedelta
 
 from config import (
-    JWT_SECRET,
-    JWT_ALGORITHM
+JWT_SECRET,
+JWT_ALGORITHM
 )
 
-
 def create_token(
-    user_id: int
+user_id: int
 ):
 
-    payload = {
-        "user_id": user_id,
-        "exp": datetime.utcnow()
-        + timedelta(days=30)
-    }
+```
+payload = {
 
-    return jwt.encode(
-        payload,
-        JWT_SECRET,
-        algorithm=JWT_ALGORITHM
-    )
+    "user_id": user_id,
 
+    "exp": datetime.utcnow()
+    + timedelta(days=30)
+
+}
+
+return jwt.encode(
+    payload,
+    JWT_SECRET,
+    algorithm=JWT_ALGORITHM
+)
+```
 
 def verify_token(
-    token: str
+token: str
 ):
 
-    return jwt.decode(
+```
+try:
+
+    payload = jwt.decode(
         token,
         JWT_SECRET,
         algorithms=[
             JWT_ALGORITHM
         ]
     )
+
+    return payload
+
+except JWTError:
+
+    return None
+```
