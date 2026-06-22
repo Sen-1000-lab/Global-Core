13e1cc5a32f22523cce69de6815287eee0c6b9de
Sen-xSpk-lab <t31075049@nara-su.ac.jp>
2026-06-23 08:11:45 +0900

    Update app.py

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from routes.users import router as user_router
 from routes.friends import router as friend_router
 from routes.blocks import router as block_router
+from routes.favorites import router as favorite_router
 
 app = FastAPI(
 title="Globally Core API"
@@ -24,6 +25,12 @@ app.include_router(
 block_router,
 prefix="/api/blocks",
 tags=["Blocks"]
+)
+
+app.include_router(
+favorite_router,
+prefix="/api/favorites",
+tags=["Favorites"]
 )
 
 @app.get("/")
